ed1ea698a758371c30a34ba5f1ee43182b919289
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-10-18 19:53:16 +0300

    Create adjacency-matrix.ts

---
+++ graph-based/adjacency-matrix.ts
@@ -0,0 +1,68 @@
+/*
+    Adjacency Matrix
+
+    An adjacency matrix is a square matrix used in graph theory to represent connections between nodes in a graph, with 1s indicating the presence of edges and 0s indicating their absence.
+
+    In Real Life:
+        1. Social Networks: An adjacency matrix can be used to represent a social network. Each node in the graph represents a person, and each edge represents a connection between two people.
+        2. Computer Networks: An adjacency matrix can be used to represent a computer network. Each node in the graph represents a computer, and each edge represents a connection between two computers.
+        3. Image Processing: An adjacency matrix can be used to represent an image. Each node in the graph represents a pixel, and each edge represents a connection between two pixels.
+
+    Description and comments created by chatgpt and github copilot
+ */
+
+class Matrix{
+    private matrix: number[][] = [];
+
+    public constructor(size: number) {
+        for(let i = 0; i < size; i++){
+            this.matrix.push([]);
+            for(let j = 0; j < size; j++){
+                this.matrix[i].push(0);
+            }
+        }
+    }
+
+    public addEdge(from: number, to: number): void
+    {
+        this.matrix[from][to] = 1;
+        this.matrix[to][from] = 1;
+    }
+
+    public removeEdge(from: number, to: number): void
+    {
+        this.matrix[from][to] = 0;
+        this.matrix[to][from] = 0;
+    }
+
+    public getMatrix(): number[][]
+    {
+        return this.matrix;
+    }
+}
+
+var myMatrix = new Matrix(5);
+
+myMatrix.addEdge(0, 1);
+myMatrix.addEdge(0, 2);
+myMatrix.addEdge(1, 2);
+myMatrix.addEdge(2, 0);
+myMatrix.addEdge(2, 3);
+console.log(myMatrix.getMatrix());
+// [
+//   [0, 1, 1, 0, 0],
+//   [1, 0, 1, 0, 0],
+//   [1, 1, 0, 1, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 0, 0, 0]
+// ]
+
+myMatrix.removeEdge(0, 1);
+console.log(myMatrix.getMatrix());
+// [
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [1, 1, 0, 1, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 0, 0, 0]
+// ]
