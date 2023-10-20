8cc6aa4094aa68f4f4cb3e12d3b9d78340c936f0
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-10-20 18:27:43 +0300

    Create bellman_ford.ts

---
+++ graph-based/bellman_ford.ts
@@ -0,0 +1,11 @@
+/*
+
+    Bellman-Ford Algorithm
+
+    The Bellman-Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph. It is slower than Dijkstra's algorithm for the same problem, but more versatile, as it is capable of handling graphs in which some of the edge weights are negative numbers.
+
+    In Real Life:
+        1. Routing Algorithms: Bellman-Ford is used by routing protocols to compute the shortest path between the source and destination nodes.
+
+    Description and comments created by chatgpt and github copilot
+ */
