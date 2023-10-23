394bcf5952abb636a98ba3a211991e47a0013909
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-10-23 16:28:35 +0300

    Create dijkstra.ts

---
+++ greedy-algorithms/dijkstra.ts
@@ -0,0 +1,12 @@
+/*
+    Dijkstra Algorithm
+
+    Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks. It was conceived by computer scientist Edsger W. Dijkstra in 1956 and published three years later.
+
+    In Real Life:
+        1. Google Maps: Finding the shortest path from your location to your destination.
+        2. Network Routing: Finding open shortest path for data transfer.
+        3. Biology: Used to model the spread of viruses among humans.
+
+    Description and comments created by chatgpt and github copilot
+ */
